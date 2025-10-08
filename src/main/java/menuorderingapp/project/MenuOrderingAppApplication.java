925dbbe13e41e408c5f6d32c4328b837239cfcec
XDX1O1 <gendhiramona@gmail.com>
2025-10-08 16:25:30 +0700

    First Commit

---
+++ src/main/java/menuorderingapp/project/MenuOrderingAppApplication.java
@@ -0,0 +1,13 @@
+package menuorderingapp.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuOrderingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuOrderingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/menuorderingapp/project/MenuOrderingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package menuorderingapp.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuOrderingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
